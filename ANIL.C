7f8edc7adafb241e9dd4278526c641269c1e1899
25kq1a4790-dev <25kq1a4790@pace.ac.in>
2025-11-01 15:28:20 +0530

    Add files via upload

---
+++ ANIL.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+ void main()
+ {
+  int a,b,sum;
+  int addition,division,sub,multiplication;
+  clrscr();
+  printf("enter a,b values");
+  scanf("%d%d",&a,&b);
+  sum=a+b;
+  sum=a-b;
+  multiplication=a*b;
+  division=a/b;
+  printf("%d\n",sum);
+  printf("%d\n",sub);
+  printf("%d\n",multiplication);
+  printf("%d\n",division);
+  getch();
+  }
